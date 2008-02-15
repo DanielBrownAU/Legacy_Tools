43ba99a13004b5102b82a7100b4a1761852f8e1a
DanielBrown82_cp <SND\DanielBrown82_cp>
2008-02-15 22:23:48 +0000

    Initial Checkin

---
+++ DanielBrown.Tools/DanielBrown.Tools.ActiveDirectory/ActiveDirectoryDetector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DanielBrown.Tools.ActiveDirectory
+{
+    class ActiveDirectoryDetector
+    {
+        public static bool IsDomainMember()
+        {
+            return false;
+        }
+
+        public static bool IsDomainController()
+        {
+            return false;
+        }
+    }
+}
