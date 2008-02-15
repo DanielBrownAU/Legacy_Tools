b2d973381f45eb4d1981c4515cad5d622cd1fc98
DanielBrown82_cp <SND\DanielBrown82_cp>
2008-02-15 23:57:27 +0000

    Code #1

---
+++ DanielBrown.Tools/Impersonation/LogonException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DanielBrown.Tools.Impersonation
+{
+    public class LogonException : Exception
+    {
+        public LogonException(string Message)
+            : base(Message)
+        {
+        }
+
+        public LogonException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
